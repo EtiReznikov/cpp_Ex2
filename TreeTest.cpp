29f1e1798ec5936175afa2e7abb4b640b653cde2
etireznikov <etirez56@gmail.com>
2019-03-21 12:36:29 +0200

    tests

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -68,6 +68,8 @@ int main() {
   .CHECK_THROWS(mytree2.print())
   .CHECK_THROWS(mytree2.root())
 
+
+
   .print();
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
